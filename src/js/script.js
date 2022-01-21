63c865cbdb2281bed252d6186818ae07daa8bf5e
Emilia <81705094+ccryptocoder@users.noreply.github.com>
2022-01-21 19:22:57 +0600

    catalog item animation

--- src/js/script.js
+++ src/js/script.js
@@ -22,5 +22,46 @@ $(document).ready(function(){
             }
         ]
       });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      $('.catalog-item__link').each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          });
+      });
+
+      $('.catalog-item__back').each(function(i) {
+          $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          });
+      });
+
+      /* const moreLinks = Array.from(document.querySelectorAll('.catalog-item__link'));
+      const backLinks = Array.from(document.querySelectorAll('.catalog-item__back'));
+      const catalogItemMainPages = Array.from(document.querySelectorAll('.catalog-item__content'));
+      const catalogItemBackPages = Array.from(document.querySelectorAll('.catalog-item__list'));
+
+      moreLinks.forEach((link, index) => {
+        link.addEventListener('click', () => {
+            catalogItemMainPages[index].classList.toggle('catalog-item__content_active');
+            catalogItemBackPages[index].classList.toggle('catalog-item__list_active');
+        })
+      })
+
+      backLinks.forEach((link, index) => {
+        link.addEventListener('click', () => {
+            catalogItemMainPages[index].classList.toggle('catalog-item__content_active');
+            catalogItemBackPages[index].classList.toggle('catalog-item__list_active');
+        })
+      }) */
   });
           
