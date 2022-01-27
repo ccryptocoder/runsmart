adcda881a62c59763e61be89c36315469411ea04
Emilia <81705094+ccryptocoder@users.noreply.github.com>
2022-01-27 19:24:56 +0600

    replace tel -> phone

--- src/js/script.js
+++ src/js/script.js
@@ -88,7 +88,7 @@ $(document).ready(function(){
 	  validateForm('#consultation form');
 	  validateForm('#order form');
 
-	  $('input[name=tel]').mask('+7 (999) 999-99-99');
+	  $('input[name=phone]').mask('+7 (999) 999-99-99');
 
 
 	  /* $('.button_submit').on('click', function() {
