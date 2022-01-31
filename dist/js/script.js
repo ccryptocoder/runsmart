34aa12c5ca962c6a59ddf9b7b7595ccfa3bcc930
Emilia <81705094+ccryptocoder@users.noreply.github.com>
2022-01-31 13:37:12 +0600

    Add files to dist

---
+++ dist/js/script.js
@@ -0,0 +1,146 @@
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+		speed: 1000,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="icons/left_arrow.png" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="icons/right_arrow.png" alt=""></button>',
+		/* adaptiveHeight: true, */
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					dots: true
+				},
+				breakpoint: 767,
+				settings: {
+					autoplay: false,
+					arrows: false,
+					dots: true,
+				}
+			}
+		]
+	  });
+
+	  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+		  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	  });
+
+	  function toggleSlide(className) {
+		$(className).each(function(i) {
+			$(this).on('click', function(e) {
+			  e.preventDefault();
+			  $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+			  $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			});
+		});
+	  }
+
+	  toggleSlide('.catalog-item__back');
+	  toggleSlide('.catalog-item__link');
+
+	  //modal
+
+	  $('[data-modal=consultation]').on('click', function() {
+		  $('.overlay, #consultation').fadeIn();
+	  }) ;
+	  $('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #order, #thanks').fadeOut();
+	  });
+
+	  $('.button_mini').each(function(i) {
+		  $(this).on('click', function() {
+			$('.overlay, #order').fadeIn();
+			$('#order__descr').text($('.catalog-item__subtitle').eq(i).text());
+		  })
+	  })
+
+	  /* $('.feed-form').each(function() {
+		  $(this).validate();
+	  }) */
+
+	  function validateForm(form) {
+		$(form).validate({
+			rules: {
+				name: 'required',
+				tel: 'required',
+				email: {
+				  required: true,
+				  email: true
+				}
+			},
+			messages: {
+				name: 'Это поле обязательно',
+				tel: 'Это поле обязательно',
+				email: {
+					required: 'Это поле обязательно',
+					email: 'Некорректный email адрес'
+				}
+  
+			}
+		});
+	  }
+
+	  validateForm('#consultation-form');
+	  validateForm('#consultation form');
+	  validateForm('#order form');
+
+	  $('input[name=phone]').mask('+7 (999) 999-99-99');
+
+	  $('form').submit(function(e) {
+		e.preventDefault();
+
+		if(!$(this).valid()) {
+			return; //если форма не прошла валидацию, она не отправится
+		}
+
+		$.ajax({
+			type: 'POST',
+			url: 'mailer/smart.php',
+			data: $(this).serialize(),
+		}).done(function() {
+			$(this).find('input').val('');
+			
+			$('#order, #consultation').fadeOut(); 
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	  });
+
+	  // pageup
+
+	  $(window).scroll(function() {
+		  if ($(this).scrollTop() > 1200) {
+			  $('.pageup').fadeIn();
+		  }else {
+			  $('.pageup').fadeOut();
+		  }
+	  });
+
+	  new WOW().init();
+
+	/* const moreLinks = Array.from(document.querySelectorAll('.catalog-item__link'));
+	const backLinks = Array.from(document.querySelectorAll('.catalog-item__back'));
+	const catalogItemMainPages = Array.from(document.querySelectorAll('.catalog-item__content'));
+	const catalogItemBackPages = Array.from(document.querySelectorAll('.catalog-item__list'));
+
+	function toggleSlide(linkElements) {
+		linkElements.forEach((link, index) => {
+			link.addEventListener('click', (event) => {
+				event.preventDefault();
+				catalogItemMainPages[index].classList.toggle('catalog-item__content_active');
+				catalogItemBackPages[index].classList.toggle('catalog-item__list_active');
+			})
+		})
+	}
+
+	toggleSlide(moreLinks);
+	toggleSlide(backLinks); */
+
+  });
+		  
